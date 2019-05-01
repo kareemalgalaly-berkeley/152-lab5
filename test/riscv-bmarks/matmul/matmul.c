226588753ad44ce571670ea0b83ec6090bdf306f
George Higgins Hutchinson <hutch@eecs.berkeley.edu>
2019-05-01 15:56:27 -0700

    Added initial blocked matmul

--- test/riscv-bmarks/matmul/matmul.c
+++ test/riscv-bmarks/matmul/matmul.c
@@ -4,6 +4,8 @@
 #include "util.h"
 #include <stddef.h>
 
+// static data_t;
+
 void matmul(const size_t coreid, const size_t ncores, const size_t lda,  const data_t A[], const data_t B[], data_t C[])
 {
   size_t i, j, k;
@@ -20,4 +22,20 @@ void matmul(const size_t coreid, const size_t ncores, const size_t lda,  const d
 
 void matmul_opt(const size_t coreid, const size_t ncores, const size_t lda,  const data_t A[], const data_t B[], data_t C[])
 {
+    size_t i, j, k;
+    
+    size_t start_j = lda / ncores * coreid;
+    size_t end_j   = (coreid == ncores - 1) ? lda : lda / ncores * (coreid + 1);
+
+//    memset(C, 0, lda*lda);
+
+    for (j = start_j; j < end_j; j++) {
+        for (i = 0; i < lda; i++) {
+            data_t sum = 0;
+            for (k = 0; k < lda; k++)
+                sum += A[j*lda + k] * B[k*lda + i];
+            C[i + j*lda] = sum;
+        }
+    }
 }
+
